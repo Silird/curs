717aed7a89b2b765e61fb53ae312fce61ba3f6b0
Silird <Silird@gmail.com>
2015-12-19 12:56:25 +0300

    Test

---
+++ src/main/java/util/Records.java
@@ -0,0 +1,29 @@
+package util;
+
+import javax.swing.table.DefaultTableModel;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Records {
+    Set record;
+    DefaultTableModel model;
+
+    public Records(DefaultTableModel m) {
+        model = m;
+        record = new HashSet();
+
+    }
+
+    public void add() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void edit() {
+
+    }
+
+}
